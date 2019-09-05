a950fc094ad29b03bbe988e9b88083998c529c9a
Kelen Y. Yafuso <kyyafuso@gmail.com>
2019-09-05 09:34:25 -0700

    Still working on styling, not perfect

--- src/topspot.jsx
+++ src/topspot.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default props => (
-          <div className='well'>
-              <h4>{props.name}</h4>
-              <p>{props.description}</p>
-              <a role='button' className='btn btn-primary' href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`}>Go!</a>
+          <div className='well' style={{display:'flex', flexFlow:'row wrap', justifyContent:'space-around'}}>
+              <h4 className='card' style={{backgroundColor: '#25DDF2', textAlign:'center', width:'80%'}}>{props.name}</h4>
+              <p className='card' style={{backgroundColor:'#25DDF2', textAlign:'center', width:'80%'}}>{props.description}</p>
+              <a role='button' className='card btn btn-primary' style={{width:'80%', color:'black', textAlign:'center', backgroundColor:'#25DDF2'}} href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`}>Go!</a>
           </div>
         );
